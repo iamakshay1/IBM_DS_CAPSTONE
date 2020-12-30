4676c18a238d3031c336e226db0b5996b703879d
iamakshay1 <akshayz1806@gmail.com>
2020-12-30 14:47:43 +0530

    Final_Commit
    Final Changes done on Local.

---
+++ Project.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#this will be used for capstone project
+
+
+# In[2]:
+
+
+import numpy as np
+
+
+# In[3]:
+
+
+import pandas as pd
+
+
+# In[ ]:
+
+print("Hello Capstone Project")
